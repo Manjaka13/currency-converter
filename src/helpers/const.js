711d471e9934f075f6ac67b5ebe0a762288213a0
Hari <manjaka.rajaonson@gmail.com>
2022-07-27 19:15:33 +0300

    add offline data mode

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -2,12 +2,14 @@
     Export constants
 */
 
+const localMode = process.env.LOCAL_MODE || false;
 const developmentMode = process.env.DEVELOPMENT;
 const port = process.env.PORT || 3001;
 const currencyApiKey = process.env.CURRENCY_API || "";
-const baseUrl = process.env.BASE_URL || "";
+const baseUrl = localMode ? `http://localhost:${port}` : (process.env.BASE_URL || "");
 
 module.exports = {
+    localMode,
     developmentMode,
     port,
     currencyApiKey,
--- src/services/currency.js
+++ src/services/currency.js
@@ -1,5 +1,7 @@
 const https = require("https");
-const { baseUrl, currencyApiKey } = require("../helpers/const");
+const { baseUrl, currencyApiKey, localMode } = require("../helpers/const");
+const localConvert = require("../../data/convert.json");
+const localCurrenciesList = require("../../data/currenciesList.json");
 
 /*
     Service for currency module
@@ -8,58 +10,66 @@ const { baseUrl, currencyApiKey } = require("../helpers/const");
 const currencyService = {
     // get list of currencies
     getCurrencyList: () => new Promise((resolve, reject) => {
-        const options = {
-            host: baseUrl,
-            path: "/exchangerates_data/symbols",
-            method: "GET",
-            headers: {
-                "apikey": currencyApiKey
-            }
-        };
-
-        https.get(options, res => {
-            let body = "";
-            res.on("data", chunk => {
-                body += chunk;
-            });
-            res.on("end", () => {
-                try {
-                    resolve(JSON.parse(body).symbols);
-                } catch (e) {
-                    reject(e);
+        if (localMode)
+            resolve(Object.keys(localCurrenciesList.symbols));
+        else {
+            const options = {
+                host: baseUrl,
+                path: "/exchangerates_data/symbols",
+                method: "GET",
+                headers: {
+                    "apikey": currencyApiKey
                 }
+            };
+
+            https.get(options, res => {
+                let body = "";
+                res.on("data", chunk => {
+                    body += chunk;
+                });
+                res.on("end", () => {
+                    try {
+                        resolve(JSON.parse(body).symbols);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            }).on("error", (e) => {
+                reject(e);
             });
-        }).on("error", (e) => {
-            reject(e);
-        });
+        }
     }),
 
     // Converts two currencies
     convert: (from, to) => new Promise((resolve, reject) => {
-        const options = {
-            host: baseUrl,
-            path: `/exchangerates_data/convert?to=${to}&from=${from}&amount=1`,
-            method: "GET",
-            headers: {
-                "apikey": currencyApiKey
-            }
-        };
-
-        https.get(options, res => {
-            let body = "";
-            res.on("data", chunk => {
-                body += chunk;
-            });
-            res.on("end", () => {
-                try {
-                    resolve(JSON.parse(body).result);
-                } catch (e) {
-                    reject(e);
+        if (localMode)
+            resolve(localConvert.result);
+        else {
+            const options = {
+                host: baseUrl,
+                path: `/exchangerates_data/convert?to=${to}&from=${from}&amount=1`,
+                method: "GET",
+                headers: {
+                    "apikey": currencyApiKey
                 }
+            };
+
+            https.get(options, res => {
+                let body = "";
+                res.on("data", chunk => {
+                    body += chunk;
+                });
+                res.on("end", () => {
+                    try {
+                        resolve(JSON.parse(body).result);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            }).on("error", (e) => {
+                reject(e);
             });
-        }).on("error", (e) => {
-            reject(e);
-        });
+        }
     })
 };
 
